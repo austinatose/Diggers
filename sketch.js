e3fa966235391a8a0396cfa480bbde9b2cce00c0
vs358 <sriramvenky24@gmail.com>
2024-04-02 16:11:30 +0800

    poop
    tiles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let ground, tilesGroup;
+
 function setup() {
-  createCanvas(1400, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight);
+
+  ground = new Group()
+  ground.w = window.innerWidth/15;
+  ground.h = ground.w;
+  ground.tile = "=";
+
+  tilesGroup = new Tiles(
+    [
+        "======.=======",
+        "=..===.=======",
+        "==============",
+        "==============",
+        "=============="
+
+    ],
+    ground.w,
+    window.innerHeight/2,
+    ground.w + 1,
+    ground.h + 1
+  );
 }
 
 function draw() {
   background(200);
 }
+
